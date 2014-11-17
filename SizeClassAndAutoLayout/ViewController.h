127af1aacdcea32c99692e0a28bbf3c87c42cf27
Tommy <(null)>
2014-11-17 14:15:09 +0800

    Initial Commit

---
+++ SizeClassAndAutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SizeClassAndAutoLayout
+//
+//  Created by HMT on 14/11/17.
+//  Copyright (c) 2014年 MTH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
